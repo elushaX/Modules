eae8852832876c3fa8f606b9c6876f2b15423386
IlushaShurupov <ilusha.main@gmail.com>
2023-06-30 23:05:38 +0300

    Strings Initial

---
+++ Strings/private/Strings.cpp
@@ -0,0 +1,24 @@
+
+#include "Strings.hpp"
+#include "Allocators.hpp"
+#include "Logging.hpp"
+
+using namespace tp;
+
+bool initialize(const ModuleManifest*) {
+	Logger::initializeGlobal();
+	return true;
+}
+
+void deinitialize(const ModuleManifest*) {
+	Logger::deinitializeGlobal();
+}
+
+static tp::ModuleManifest* sModuleDependencies[] = {
+	&tp::gModuleContainers,
+	&tp::gModuleAllocators,
+	&tp::gModuleUtils,
+	nullptr
+};
+
+ModuleManifest tp::gModuleStrings = ModuleManifest("Strings", initialize, deinitialize, sModuleDependencies);
