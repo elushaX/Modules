3e9365b1eee65521d1cb3a9915d0f0b0dfe334fd
IlyaShurupov <ilusha.main@gmail.com>
2023-10-16 22:38:45 +0300

    Adding scene files

---
+++ RayTracer/applications/rsc/cube/script.lua
@@ -0,0 +1,8 @@
+
+Meshes = "meshes.obj"
+
+Camera = {
+	pos = { 4, 4, 4 },
+	size_x = 400,
+	size_y = 600,
+}
---
+++ RayTracer/applications/rsc/scene/script.lua
@@ -0,0 +1,8 @@
+
+Meshes = "meshes.obj"
+
+Camera = {
+	pos = { 0.5, 4.5, 0.2 },
+	size_x = 500,
+	size_y = 700,
+}
