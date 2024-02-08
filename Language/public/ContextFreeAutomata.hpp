2ebf650381ac66addc3f15cb558caeadf518f9fd
IlyaShurupov <ilusha@workspace.com>
2024-02-08 10:52:28 +0300

    refactor regular automata

--- Language/public/ContextFreeAutomata.hpp
+++ Language/public/ContextFreeAutomata.hpp
@@ -10,6 +10,13 @@ namespace tp {
 	template <typename tAlphabetType, typename tStateType>
 	class ContextFreeAutomata {
 
+		struct Action {
+			enum Type { SHIFT, REDUCE, TRAP } type = TRAP;
+			ualni num = 0; // state to shift (shift action) or pop count (reduce action)
+		};
+
+		Buffer2D<Action> mTable;
+
 	public:
 		ContextFreeAutomata() = default;
 
